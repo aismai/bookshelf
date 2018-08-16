7181ced92ffe007421cb90ec761eae6dd5d5a016
aismai <abai.ismailov@gmail.com>
2018-08-16 12:05:26 +0600

    add Book view component

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -16,3 +16,35 @@ export function getBooks(limit = 10, start = 0, order = "asc", list = []) {
     payload: request
   };
 }
+
+export function getBookWithReviewer(id) {
+  const request = axios.get(`/api/book?id=${id}`);
+
+  return dispatch => {
+    request.then(({ data }) => {
+      let book = data;
+
+      axios.get(`/api/getReviewer?id=${book.ownerId}`).then(({ data }) => {
+        let response = {
+          book,
+          reviewer: data
+        };
+
+        dispatch({
+          type: "GET_BOOK_WITH_REVIEWER",
+          payload: response
+        });
+      });
+    });
+  };
+}
+
+export function clearBookWithReviewer() {
+  return {
+    type: "CLEAR_BOOK_WITH_REVIEWER",
+    payload: {
+      book: {},
+      reviewer: {}
+    }
+  };
+}
